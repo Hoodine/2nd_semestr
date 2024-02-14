7ca9596d983f6e0254fe8171315f778ebe8d1553
Hoodine <94785358+Hoodine@users.noreply.github.com>
2024-02-14 17:13:44 +0500

    Refactoring

--- main.c
+++ main.c
@@ -1,24 +1,8 @@
 #include "libs/data_structures/vector/vectorVoid.h"
-#include <assert.h>
 #include <stdio.h>
 
 int main() {
-    size_t n;
-    scanf("%zd", &n);
-
-    vectorVoid v = createVectorV(0, sizeof(int));
-
-    for (int i = 0; i < n; i++) {
-        int x;
-        scanf("%d", &x);
-        pushBackV(&v, &x);
-    }
-
-    for (int i = 0; i < n; i++) {
-        int x;
-        getVectorValueV(&v, i, &x);
-        printf("%d ", x);
-    }
+    printf("I love programming");
 
     return 0;
 }
