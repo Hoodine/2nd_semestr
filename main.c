fedd5bbfe86ceda1c0b2c04ba3e6faf63831fe05
Hoodine <94785358+Hoodine@users.noreply.github.com>
2024-05-06 14:20:18 +0500

    Solved 2nd task from 20th lab
    + isIndex func
    + calculateCountNeighbors func
    + cellResultByPosition func
    + secondTask
    + tests

--- main.c
+++ main.c
@@ -6,6 +6,36 @@
 #include "libs/data_structures/matrix/matrix.h"
 #include "libs/data_structures/vector/vector.h"
 
+typedef struct domain {
+    size_t visits;
+    char name[200];
+} domain;
+
+typedef struct node {
+    int key;
+    struct node *left;
+    struct node *right;
+} node;
+
+node *createNode(int k) {
+    node *new_node = (node *) malloc(sizeof(node));
+    new_node->key = k;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return new_node;
+}
+
+node *insert(node *p, int k, bool isLeft) {
+    node *new_node = createNode(k);
+    if (isLeft)
+        p->left = new_node;
+    else
+        p->right = new_node;
+
+    return new_node;
+}
+
 void firstTask(matrix *m, size_t countRequests, size_t *requestsArray[]) {
     size_t row1, col1, row2, col2;
     for (size_t request = 0; request < countRequests; request++) {
@@ -13,9 +43,49 @@ void firstTask(matrix *m, size_t countRequests, size_t *requestsArray[]) {
         col1 = requestsArray[request][1];
         row2 = requestsArray[request][2];
         col2 = requestsArray[request][3];
-        for (size_t indRow = row1; indRow <= row2; indRow++)
-            for (size_t indCol = col1; indCol <= col2; indCol++)
-                m->values[indRow][indCol]++;
+        for (size_t idx_row = row1; idx_row <= row2; idx_row++)
+            for (size_t idx_col = col1; idx_col <= col2; idx_col++)
+                m->values[idx_row][idx_col]++;
+    }
+}
+
+bool isIndex(size_t rows, size_t cols, int idx_row, int idx_col) {
+    if (idx_row > -1 && idx_row < rows && idx_col > -1 && idx_col < cols)
+        return true;
+
+    return false;
+}
+
+size_t calculateCountNeighbors(matrix m, int idx_row, int idx_col, size_t
+rows, size_t cols) {
+    size_t count_neighbors = 0;
+    for (int idx_row_math = idx_row - 1; idx_row_math < idx_row + 2; idx_row_math++) {
+        for (int idx_col_math = idx_col - 1; idx_col_math < idx_col + 2; idx_col_math++) {
+            if (isIndex(rows, cols, idx_row_math, idx_col_math) &&
+                m.values[idx_row_math][idx_col_math] &&
+                (idx_row != idx_row_math || idx_col != idx_col_math)) {
+                count_neighbors++;
+            }
+        }
+    }
+
+    return count_neighbors;
+}
+
+int cellResultByPosition(matrix m, int idx_row, int idx_col, size_t countNeighbors) {
+    return ((m.values[idx_row][idx_col] == 1 && (countNeighbors == 2 || countNeighbors == 3))
+            || (m.values[idx_row][idx_col] == 0 && countNeighbors == 3));
+}
+
+void secondTask(matrix m, matrix *newM, size_t rows, size_t cols) {
+    for (int idx_row = 0; idx_row < rows; idx_row++) {
+        for (int idx_col = 0; idx_col < cols; idx_col++) {
+            size_t count_neighbors =
+                    calculateCountNeighbors(m, idx_row, idx_col, rows, cols);
+
+            newM->values[idx_row][idx_col] =
+                    cellResultByPosition(m, idx_row, idx_col, count_neighbors);
+        }
     }
 }
 
@@ -25,11 +95,11 @@ void test_firstTask() {
                                                0, 0, 0,
                                                0, 0, 0},
                                        3, 3);
-    size_t countRequests = 2;
-    size_t firstRequest[4] = {1, 1, 2, 2};
-    size_t secondRequest[4] = {0, 0, 1, 1};
-    size_t *requests[2] = {firstRequest, secondRequest};
-    firstTask(&got, countRequests, requests);
+    size_t count_requests = 2;
+    size_t first_request[4] = {1, 1, 2, 2};
+    size_t second_request[4] = {0, 0, 1, 1};
+    size_t *requests[2] = {first_request, second_request};
+    firstTask(&got, count_requests, requests);
     matrix expected = createMatrixFromArray((int[]) {
                                                     1, 1, 0,
                                                     1, 2, 1,
@@ -39,8 +109,31 @@ void test_firstTask() {
     assert(areTwoMatricesEqual(&got, &expected));
 }
 
+void test_SecondTask() {
+    size_t rows = 4;
+    size_t cols = 3;
+    matrix start = createMatrixFromArray((int[]) {
+                                                 0, 1, 0,
+                                                 0, 0, 1,
+                                                 1, 1, 1,
+                                                 0, 0, 0},
+                                         4, 3);
+    matrix got = getMemMatrix(rows, cols);
+    secondTask(start, &got, rows, cols);
+    matrix expected = createMatrixFromArray((int[]) {
+                                                    0, 0, 0,
+                                                    1, 0, 1,
+                                                    0, 1, 1,
+                                                    0, 1, 0},
+                                            4, 3);
+
+    assert(areTwoMatricesEqual(&got, &expected));
+}
+
+
 int main() {
     test_firstTask();
+    test_SecondTask();
 
     return 0;
 }
